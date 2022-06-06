f757de9bb926fcf3a35860146912d5ad33c66b5f
Jenny Ryan <jenny@jennyryan.net>
2022-06-06 14:43:56 -0700

    finiah Community page

--- ouinet.work-backup/html-update-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Usage: ./html-update-all.sh
-# Update HTML pages for all usable translations.
-
-set -e
-for t in $(./translations.sh); do
-    ./html-update.sh "$t"
-done
--- ouinet.work-backup/translations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Usage: ./translations.sh
-# List the languages with usable translations.
-
-for index in $(find . -name index.po); do
-    if [ -f "${index%%.po}.html" ]; then
-        echo "$(basename $(dirname "$index"))"
-    fi
-done
